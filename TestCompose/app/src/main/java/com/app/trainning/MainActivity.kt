1b07a43df3e3365e6bd326547d7f2a501325910a
Arnold Ngassam <ngassam.arnold@nanobnk.com>
2023-04-05 21:25:05 +0400

    First commit

--- TestCompose/app/src/main/java/com/app/trainning/MainActivity.kt
+++ TestCompose/app/src/main/java/com/app/trainning/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.app.trainning
 
+import LemonTextAndImage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.app.trainning.dto.Lemon
 import com.app.trainning.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LemonadeApp()
                 }
             }
         }
@@ -30,14 +34,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LemonadeApp(){
+    initLemonApp(modifier= Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun initLemonApp(modifier:Modifier= Modifier){
+    var numberRadom= 0
+    var lemon:Lemon
+    var step by remember { mutableStateOf(1) }
+    lemon = when(step){
+        1-> Lemon(title =stringResource(R.string.tap_lemon), image = R.drawable.lemon_tree)
+        2-> {
+            if(numberRadom==0) numberRadom = (2..4).random()
+            Lemon(title =stringResource(R.string.keep_tapping), image = R.drawable.lemon_squeeze)
+        }
+        3-> Lemon(title =stringResource(R.string.lemonade_to_drink), image = R.drawable.lemon_drink)
+        else -> Lemon(title =stringResource(R.string.start_again), image = R.drawable.lemon_restart)
+    }
+
+    val imageClick: () -> Unit ={
+         when(step){
+             1, 3-> step++
+             2-> {
+                 numberRadom--
+                 if (numberRadom == 0) {
+                     step++
+                 }
+             }
+            else-> step=1
+         }
+    }
+    LemonTextAndImage(lemon,modifier=modifier,onImageClick=imageClick)
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     TestComposeTheme {
-        Greeting("Android")
+        LemonadeApp()
     }
 }
